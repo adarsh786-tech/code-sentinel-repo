6d7b0318b5bb20bd0b780914c120a24523ff0e11
Mihir Adarsh <52563368+adarsh786-tech@users.noreply.github.com>
2026-03-29 14:51:33 +0530

    Add hardcoded password to client.py
    Added hardcoded password for testing purposes.

--- api/client.py
+++ api/client.py
@@ -3,9 +3,10 @@
 import requests
 
 # VULNERABILITY: hardcoded API key
+" Vulnerability is there"
 API_KEY = "sk-prod-abc123xyz789secretkey"
 BASE_URL = "https://api.example.com"
-
+PASSWORD = "testPassword@12345"
 def get_data(endpoint):
     response = requests.get(f"{BASE_URL}/{endpoint}",
                            headers={"Authorization": f"Bearer {API_KEY}"})
